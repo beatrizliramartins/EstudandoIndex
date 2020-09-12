37985bbda20001eb497110167365ca06f3127c3f
Beatriz Martins <blmartins017@gmail.com>
2020-09-12 18:55:57 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IndexDeNovo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = ObterNomes();
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                for (int j = 0; j < nomes[i].Length; j++)
+                {
+                    if (nomes[i][j] == ' ')
+                    {
+                        break;
+                    }
+                    Console.Write(nomes[i][j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+       
+        static string[] ObterNomes()
+        {
+            string[] nomes = {
+              "Alexandra Rayane Farias Santos Silva",
+              "Cleonice Valéria de Oliveira Celestino",
+              "Jerson Luiz Silva de Brito",
+              "Beatriz Lira Martins"
+            };
+            return nomes;
+        }
+    }
+}
